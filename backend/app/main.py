a64dd5be1d48ba4e97de9c2ea3f6fc4624a22701
Balabekian Nazeli <nazeli.balabekian@hva.nl>
2024-12-21 15:09:50 +0100

    Correction

--- backend/app/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from app import models, database
-from app.routes import tasks
-
-app = FastAPI()
-
-models.Base.metadata.create_all(bind=database.engine)
-
-app.include_router(tasks.router)
